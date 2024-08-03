1dff38e5ca49299cc055f9375c850bea868abe10
horizontechsols <horizontechnologisolutions@gmail.com>
2024-08-03 11:28:00 +0530

    Sending message to both HTS and GAC

--- send_skype_message.py
+++ send_skype_message.py
@@ -5,5 +5,9 @@ from skpy import Skype
 sk = Skype("8660558126", "Motli@321", "./.tokens-app")
 
 ch = sk.contacts['live:.cid.7c28c93f39dd4afd'].chat
-print('Sending message ' + sys.argv[1] + ' to gac.hrh and horizontechnologisulutions')
+print('Sending message ' + sys.argv[1] + ' to horizontechnologisulutions')
+ch.sendMsg(sys.argv[1]);
+
+ch = sk.contacts['live:.cid.a3205fabc37a796c'].chat
+print('Sending message ' + sys.argv[1] + ' to globalautocomponents')
 ch.sendMsg(sys.argv[1]);
