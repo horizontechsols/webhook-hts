25057d0306943e077852f2739f016ce400539ed7
horizontechsols <horizontechnologisolutions@gmail.com>
2024-12-31 20:32:16 +0530

    Update send_skype_message.py

--- send_skype_message.py
+++ send_skype_message.py
@@ -4,7 +4,7 @@ from skpy import Skype
 
 sk = Skype("8660558126", "Motli@321", "./.tokens-app")
 
-ch = sk.contacts['live:.cid.7c28c93f39dd4afd'].chat
+ch = sk.contacts['live:.cid.159502fd4dc304e'].chat
 print('Sending message ' + sys.argv[1] + ' to horizontechnologisulutions')
 ch.sendMsg(sys.argv[1]);
 
